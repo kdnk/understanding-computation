64e65c2d652efafc26ae55802136906ee937513a
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-09-01 20:01:07 +0900

    while

--- 02/virtual_machine.rb
+++ 02/virtual_machine.rb
@@ -41,3 +41,12 @@ Machine.new(
   ),
   { x: Boolean.new(true) }
 ).run
+
+
+Machine.new(
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  ),
+  { x: Number.new(1) }
+).run
